9b21bb4afe552951d23a37d96ac8489df2ebe2e6
Jan Szumiec <jan.szumiec@gmail.com>
2013-11-27 17:42:06 +0000

    Cleanup.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
 source 'https://rubygems.org'
 
-gem 'fog'
 gem 'gem-compiler'
--- run.rb
+++ run.rb
@@ -4,55 +4,91 @@ require 'fileutils'
 require 'set'
 require 'rubygems/installer'
 
-def process_repo(repo_addr)
-  FileUtils.mkdir_p('tmp')
-  Dir.chdir('tmp') do
-    puts repo_addr
-    `git clone #{repo_addr} --depth 1`
+TMPDIR = 'tmp'
+S3DIE  = 's3'
+
+class Repository
+  def initialize(name, url)
+    @name = name
+    @url = url
   end
-end
 
-def repo_dirs
-  Dir['tmp/*']
-end
-
-repos = YAML.load_file('config.yml')['repos']
-repos.each { |r| process_repo(r) }
-lockfiles = repo_dirs.collect do |repo_dir|
-  Bundler::LockfileParser.new(File.read(File.join(repo_dir, 'Gemfile.lock')))
-end
-
-FileUtils.mkdir_p('s3/gems')
-Dir.chdir('s3/gems') do
-  gemnames = lockfiles.inject(Set[]) do |acc, lockfile|
-    lockfile.specs.each do |spec|
-      next if spec.source.is_a?(Bundler::Source::Git)
-
-      acc << "#{spec.name}-#{spec.version}.gem"
+  def update
+    Dir.chdir(TMPDIR) do
+      system("git pull")
     end
-    acc
-  end.to_a.map do |gemname|
-    "http://rubygems.org/gems/#{gemname}"
   end
 
-  `echo #{gemnames.join(' ')} | xargs -n 1 -P 8 wget -N -q`
-
-  gems_to_build = Dir['*.gem'].inject([]) do |acc, gem|
-    spec = Gem::Installer.new(gem).spec
-    if spec.files.find { |f| /(\.c|\.h)$/.match(f) }
-      acc << gem
+  def clone
+    Dir.chdir(TMPDIR) do
+      system("git clone #{url} --depth 1")
     end
-    acc
   end
 
-  all_gems = Dir['*.gem']
+  def exists?
+    Dir.exists?(File.join(TMPDIR, @name))
+  end
 
-  gems_to_remove = all_gems - gems_to_build
-
-  `echo #{gems_to_build.join(' ')} | xargs -n 1 -P 8 bundle exec gem compile`
-  FileUtils.rm(gems_to_remove)
+  def specs
+    Dir.chdir(File.join(TMPDIR, @name)) do
+      Bundler::LockfileParser.new(File.read('Gemfile.lock')).specs
+    end
+  end
 end
 
-`gem build_index --directory s3`
+class GemStorage
+  def initialize(directory)
+    @directory = directory
+  end
 
+  def fetch_all_gems(specs)
+    fetch_gems_paralell(downloadable_gems(specs))
+  end
 
+  private
+  def downloadable_gems(specs)
+    specs.reject do |spec|
+      spec.source.is_a? Bundler::Source::Git 
+    end
+  end
+
+  def fetch_gems_paralell(specs)
+    gemurls = specs.map do |spec|
+      "http://rubygems.org/gems/" + gemname(spec)
+    end
+    
+    system("echo #{gemurls} | xargs -n 1 -P 8 wget -N -q")
+  end
+
+  def build_in_paralell(specs)
+    gemnames = buildable_gems.map do |spec|
+      gemname(spec)
+    end
+
+    system("echo #{genames) | xargs -n 1 -P 8 bundle exec gem compile")
+  end
+
+  def buildable_gems(specs)
+    specs.select do |spec|
+      filename = File.join(@directory, gemname(spec))
+      hard_spec = Gem::Installer.new(filename).spec
+      spec.find { |f| /(\.c|\.h)$/.match(f) }
+    end
+  end
+
+  def gemname(spec)
+    "#{spec.name}-#{spec.version}.gem"
+  end
+
+  def cleanup(specs)
+    gemfilenames = (specs - buildable_gems(specs)).map do |spec|
+      File.join(@directory, gemname(spec))
+    end
+
+    FileUtils.rm(gemfilename)
+  end
+
+  def build_index
+    system("gem build_index --directory s3")
+  end
+end
